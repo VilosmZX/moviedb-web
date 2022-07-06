af2bbf4cca806139a486cd53d160fb4e1e0fc1ae
VilosmZX <jravaell@gmail.com>
2022-07-06 18:54:53 +0700

    Added footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useEffect, useState } from 'react';
 import  './App.css';
 import MovieCard from './components/MovieCard';
+import Footer from './components/Footer';
 
 const API_URL = 'https://imdb8.p.rapidapi.com/auto-complete';
 
@@ -26,29 +27,32 @@ const App = () => {
     }, [searchTerm]);
 
     return (
-        <div className='app'>
-            <h1>JMovie</h1>
-            <div className='search'>
-                <input 
-                    placeholder='Search Movies'
-                    value={searchTerm}
-                    onChange={(e) => setSearchTerm(e.target.value)}
-                />
-            </div>
+        <>
+            <div className='app'>
+                <h1>JMovie</h1>
+                <div className='search'>
+                    <input 
+                        placeholder='Search Movies'
+                        value={searchTerm}
+                        onChange={(e) => setSearchTerm(e.target.value)}
+                    />
+                </div>
 
-            {movies?.length > 0
-                ? (
-                    <div className='container'>
-                        {movies.map((movie) => (
-                            <MovieCard key={movie.id} movie={movie} />
-                        ))}
-                    </div>
-                ) : (
-                    <div className='empty'>
-                        <h2>Movies not found!</h2>
-                    </div>    
-                )}
-        </div>
+                {movies?.length > 0
+                    ? (
+                        <div className='container'>
+                            {movies.map((movie) => (
+                                <MovieCard key={movie.id} movie={movie} />
+                            ))}
+                        </div>
+                    ) : (
+                        <div className='empty'>
+                            <h2>Movies not found!</h2>
+                        </div>    
+                    )}
+            </div>
+            <Footer />
+        </>
     )
 };
 
