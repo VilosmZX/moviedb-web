5167e4e0465076ec5dd90095678f7beb1097d7b7
VilosmZX <jravaell@gmail.com>
2022-07-06 18:23:40 +0700

    Handle undefined object

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,17 +4,17 @@ const MovieCard = ({ movie }) => {
     return (
         <div className='movie'>
             <div className='title'>
-                <p>{movie?.l}</p>
-                <span>{movie?.y}</span>
+                <p>{movie?.l ? movie?.l : 'No Title'}</p>
+                <span>{movie?.y ? movie?.y : 'Unknown'}</span>
             </div>
             <div>
                 <img
                     src={movie?.i ? movie?.i.imageUrl : 'https://via.placeholder.com/400'}
-                    alt={movie?.l}
+                    alt={movie?.l ? movie?.l : 'No Title'}
                 />
             </div>
             <div>
-                <span>Rank {movie?.rank}</span>
+                <span>Rank {movie?.rank ? movie?.rank : 'Unranked'}</span>
             </div>
         </div>
     );
